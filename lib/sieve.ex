54c9e7229b88637850a3160c00b498d7c3fdd8c0
Marek Kasztelnik <mkasztelnik@gmail.com>
2016-01-20 22:09:01 +0100

    Move test into doctest

--- lib/sieve.ex
+++ lib/sieve.ex
@@ -1,4 +1,13 @@
 defmodule Sieve do
+  @doc ~S"""
+  Calculate prime numbers from `2..max` range.
+
+  ## Examples
+
+    iex> Sieve.primes(100)
+    [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41,
+     43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97]
+  """
   def primes(max) when max > 1 do
     calc_primes(Enum.to_list(2..max))
   end
