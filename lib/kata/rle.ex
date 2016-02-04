0afb881d93c1804b538232c7a3d60242fa0ff86e
Marek Kasztelnik <mkasztelnik@gmail.com>
2016-02-04 17:35:08 +0100

    Documentation formatting

--- lib/kata/rle.ex
+++ lib/kata/rle.ex
@@ -1,8 +1,8 @@
 defmodule Kata.RLE do
   @doc ~S"""
-   Run-Length Encoding
+  Run-Length Encoding
 
-   ## Examples
+  ## Examples
     iex> Kata.RLE.encode([])
     []
 
@@ -17,7 +17,7 @@ defmodule Kata.RLE do
 
     iex> Kata.RLE.encode([1, 2, 2, 2, 3, 4, 4, 5, 6, 6, 6, 6])
     [1, {2, 3}, 3, {4, 2}, 5, {6, 4}]
-   """
+  """
   def encode([]), do: []
   def encode([head, head | tail]), do: encode([{head, 2} | tail])
   def encode([{head, n}, head | tail]), do: encode([{head, n + 1} | tail])
